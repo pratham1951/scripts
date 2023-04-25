e96ee33069d0477838b4c0d261653a9eacbc4b10
prathamsharma <pratham.sharma@nagarro.com>
2023-04-25 16:50:25 +0530

    first commit

---
+++ Powershell/Testing.ps1
@@ -0,0 +1,12 @@
+﻿$srcpath = "C:\Users\prathamsharma\Downloads\TestingPurpose\Subfolders1"
+$dstpath = "C:\Users\prathamsharma\Downloads\TestingPurpose\Subfolders2"
+
+for($i=1;$i -le 50;$i++)
+{
+New-Item -path $srcpath -name "TypeATest$i.txt"
+}
+for($i=51;$i -le 100;$i++)
+{
+New-Item -path $dstpath -name "TypeBTest$i.txt"
+}
+
